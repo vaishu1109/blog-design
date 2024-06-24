3e7363c536e4ce7f5e5fb8a8f3121d065b8af4a5
vaishu1109 <vaishu.sg1109@gmail.com>
2024-06-24 21:07:59 +0530

    Folder and data will be created

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const blog = [
+    {
+        title: "Using Python Web Scraping to Analyze Reddit Posts",
+        description:
+            "If you’re interested in getting a unique data set consisting of user-generated posts, Python web scraping can help you get the job done. In this article, we’ll show you how to scrape text data from the web and give you inspiration about what to do with it. Web scraping is the process of downloading data from the source code of a webpage. This data can be anything – text, images, videos, or even data in tables.",
+        author: {
+            name: "Luke Hande",
+            author_img: ""
+        },
+        date: "17 Jun 2024"
+    },
+    {
+        title: "The Best Python Books for Data Analysts",
+        description: "What role do books play in acquiring data analysis skills? Discover how you can benefit from the best Python books for data analysts – and how to use them as a supplementary learning material. Data is a highly valuable asset. In fact, we might even argue that it’s the most valuable asset in the 21st century. The success and performance of products depends on the quality of input data. However, data in its raw form does not give us its full potential.",
+        author: {
+            name: "Soner Yildril",
+            author_img: ""
+        },
+        date: "10 Jun 2024"
+    },
+    {
+        title: "Just How Valuable Is Python for Business?",
+        description:"If you're wondering how Python can reshape your business, this article is for you. We’ll look at the practical applications of using Python for Business and discuss why implementing Python tools might be a smart move for your organization. Python is definitely having a moment – or maybe a decade! Implementing Python for business uses – especially in data analysis – is transforming the way companies handle data, analytics, and automation.", 
+        author: {
+            name: "Jakub Romanowski",
+            author_img: ""
+        },
+        date: "3 Jun 2024"
+    },
+
+];
